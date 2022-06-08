4edc1bbb1d31b29bd07e7f4a7bcbf3af10ad0317
Linuschukwuma <linuschukwuma10@gmail.com>
2022-06-07 23:04:29 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,18 +8,14 @@
  */
 void print_times_table(int n)
 {
-
 	int x, y, z;
 
 	if (n >= 0 && n <= 14)
 	{
-
 		for (x = 0; x <= n; x++)
 		{
-
 			for (y = 0; y <= n; y++)
 			{
-
 				z = x * y;
 				if (z > 99)
 				{
